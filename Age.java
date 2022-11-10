0fd0b4a268e207a3b6b4164b82578fac0be15e1f
Rishi Varma Kalidindi <rishivarma.lc.dev@gmail.com>
2022-11-10 00:20:00 -0500

    Added the Java Source Code File

---
+++ Age.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Age {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        final int CUR_YEAR = 2022;
+        System.out.print("\n  Provide Your Age :  ");
+        int age = scan.nextInt();
+        System.out.println("\n  Your Birth Year is :  " + (CUR_YEAR - age));
+    }
+}
